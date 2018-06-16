416e37a22f24133643b8dff5248eaeb80ca3d556
veena <veena14cs@gmail.com>
2018-06-16 18:57:05 +0530

    added arrays

--- app/src/main/java/com/stacktips/speechtotext/dataSet/Constants.java
+++ app/src/main/java/com/stacktips/speechtotext/dataSet/Constants.java
@@ -1,5 +1,8 @@
 package com.stacktips.speechtotext.dataSet;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by apple on 16/06/18.
  */
@@ -11,7 +14,16 @@ public class Constants {
     public static String channelVerb="channel";
     public static String channelnumber="number";
 
+     public static ArrayList<String> verbArray = new ArrayList<String>(
+             Arrays.asList("Play", "Change", "Switch","Turn"));
 
+     public static ArrayList<String> channelArray=new ArrayList<>(Arrays.asList("number","name"));
+    public static ArrayList<String> favouriteArray = new ArrayList<String>(
+            Arrays.asList("favorite sport", "favorite Channel", "favorite Movie","favorite Actor"));
+
+
+    public static ArrayList<String> categoryArray = new ArrayList<String>(
+            Arrays.asList("Movie", "Sports", "Music","News"));
 
 
 }
